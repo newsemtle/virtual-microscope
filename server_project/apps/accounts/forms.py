1a6a3e64289eaa5d00f2ec63fcb1eac506f03cc7
onsuo <77667461+onsuo@users.noreply.github.com>
2025-03-17 18:30:37 +0900

    feature: removed password rule when creating user in admin site

--- server_project/apps/accounts/forms.py
+++ server_project/apps/accounts/forms.py
@@ -11,6 +11,24 @@ class UserCreationForm(auth_forms.UserCreationForm):
         fields = ("username", "email", "first_name", "last_name")
 
 
+class AdminUserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Password confirmation", widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "first_name", "last_name")
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+
 class UserChangeForm(auth_forms.UserChangeForm):
     class Meta:
         model = User
@@ -19,7 +37,3 @@ class UserChangeForm(auth_forms.UserChangeForm):
 
 class LoginForm(auth_forms.AuthenticationForm):
     remember_me = forms.BooleanField(required=False, initial=False)
-
-
-class PasswordChangeForm(auth_forms.PasswordChangeForm):
-    model = User
